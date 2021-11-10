93fdd9a05aab436931bbecfea25dc81d71716293
omar.software <79000656+Omar0538@users.noreply.github.com>
2021-11-10 11:20:03 +0300

    string to int

--- radius.js
+++ radius.js
@@ -1,4 +1,4 @@
 let pi=3.14;
 let yaricap=process.argv.slice(2);
-let daire_alan=pi*(yaricap*yaricap);
+let daire_alan=pi*((yaricap[0]*1)*(yaricap[0]*1));
 console.log(`Yarıçapı ${yaricap[0]} olan dairenin alanı: ${daire_alan} şeklinde olmalıdır.`);
